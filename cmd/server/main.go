cafeb3f5652f210b0cccd5048d1b5c93bd015c63
afmireski <afmireski@outlook.com>
2023-09-16 14:22:49 -0300

    Implement BackupSystem (untested)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/afmireski/alp-backup-system/internal/data_structures"
 )
 
 func main() {
+	src := "/home/afmireski/Área de trabalho/tmp"
+	srcDir := "tmp"
+	path := "/home/afmireski/Área de trabalho/tmp/x"
+	dst := "/home/afmireski/Área de trabalho/tmp/bkp/"
+
+	fileInfo, _ := os.Lstat(path)
+	after, _ := strings.CutPrefix(path, src) 
+	dirPath := dst + srcDir + after;
+	fmt.Println(dirPath)
+
+	os.Mkdir(dirPath, fileInfo.Mode())
+}
+
+func test2() {
+	target := "y.txt"
+	path := "/home/afmireski/Área de trabalho/tmp/"
+
+	fileInfo, err := os.Lstat(path+target)
+
+	if err != nil {
+		log.Fatal(err);
+	}
+
+	fileData, err := os.ReadFile(path+target)
+	if err != nil {
+		log.Fatal(err);
+	}
+	bPath := "/home/afmireski/Área de trabalho/tmp/bkp/z/" + target 
+
+	err = os.WriteFile(bPath, fileData, fileInfo.Mode())
+	if err != nil {
+		log.Fatal(err);
+	}
+}
+
+func test1() {
 	backupTable := data_structures.CreateBackupTable[uint](11)
 
 	paths := []string{
@@ -18,6 +57,50 @@ func main() {
 		"/home/afmireski/Documentos/training/go/fullcycle/curso-go",
 		"/home/afmireski/Documentos/isos/manjaro-kde-22.1.3-23.iso",
 		"/home/afmireski/Documentos/isos/manjaro-gnome-22.1.3.iso",
+	}
+
+	fileInfo, err := os.Lstat("/home/afmireski/Documentos/BCC/seinfos")
+
+	// cannot handle symbolic link
+	//fileInfo, err := os.Lstat("file.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Name : ", fileInfo.Name())
+
+	fmt.Println("Size : ", fileInfo.Size())
+
+	fmt.Println("Mode/permission : ", fileInfo.Mode())
+
+	// --- check if file is a symlink
+
+	if fileInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
+		fmt.Println("File is a symbolic link")
+	}
+
+	fmt.Println("Modification Time : ", fileInfo.ModTime())
+
+	fmt.Println("Is a directory? : ", fileInfo.IsDir())
+
+	fmt.Println("Is a regular file? : ", fileInfo.Mode().IsRegular())
+
+	fmt.Println("Unix permission bits? : ", fileInfo.Mode().Perm())
+
+	fmt.Println("Permission in string : ", fileInfo.Mode().String())
+
+	fmt.Println("What else underneath? : ", fileInfo.Sys())
+
+	dirs, e := os.ReadDir("/home/afmireski/Documentos/isos/")
+
+	if e == nil {
+		fmt.Println(" Len ", len(dirs))
+		for _, d := range dirs {
+			fmt.Println(" -> ", d.Name())
+		}
+	} else {
+		log.Fatal(e)
 	}
 
 	for i, p := range paths {
--- internal/backup_system/backup_system.go
+++ internal/backup_system/backup_system.go
@@ -1,7 +1,10 @@
 package backup_system
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"strings"
 
 	"time"
 
@@ -10,7 +13,7 @@ import (
 
 type BackupSystem interface {
 	Init() BackupSystem
-	SetBackup(path string) error
+	SetBackupSrc(path string) error
 	Sync() error
 }
 
@@ -26,13 +29,119 @@ func (fm FileMetadata) String() string {
 }
 
 type MyBackupSystem struct {
-	modifiedAt time.Time
-	tableData  data_structures.BackupTable[FileMetadata]
+	syncedAt      time.Time
+	backupSrc     string
+	srcDir        string
+	backupDst     string
+	backupHistory data_structures.BackupTable[FileMetadata]
 }
 
-func (mbs MyBackupSystem) Init() MyBackupSystem {
+func (mbs MyBackupSystem) Init(dst string) MyBackupSystem {
 	return MyBackupSystem{
-		modifiedAt: time.Now(),
-		tableData:  data_structures.CreateBackupTable[FileMetadata](50),
+		syncedAt:      time.Now(),
+		backupSrc:     "",
+		srcDir:        "",
+		backupDst:     dst,
+		backupHistory: data_structures.CreateBackupTable[FileMetadata](50),
 	}
+}
+
+func (mbs *MyBackupSystem) SetBackupSrc(path string) error {
+	fileInfo, err := os.Stat(path)
+
+	if err != nil {
+		return err
+	} else if !fileInfo.IsDir() {
+		return errors.New("O seu caminho de backup deve apontar para um diretório")
+	}
+
+	mbs.backupSrc = path
+	mbs.srcDir = fileInfo.Name() // Obtém o nome útil do diretório de backup
+
+	return nil
+}
+
+func (mbs *MyBackupSystem) Sync() error {
+	defer mbs.setSyncedAt()
+
+	if len(mbs.backupSrc) > 0 {
+		mbs.sync(mbs.backupSrc)
+	}
+	return nil
+}
+
+func (mbs *MyBackupSystem) sync(path string) error {
+	pathInfo, pathErr := os.Lstat(path)
+
+	if pathErr == nil { // Existe o caminho para verificar
+		// Verifica se o caminho já existe no histórico
+		_, value, existsPath := mbs.backupHistory.Search(path)
+
+		if pathInfo.IsDir() { // Verifica se o caminho aponta para um diretório
+			dirContent, dirErr := os.ReadDir(path) // Tenta ler o diretório
+
+			if dirErr == nil { // Verifica se conseguiu ler
+				if len(dirContent) > 0 { // Verifica se o diretório não está vazio
+
+					if !existsPath {
+						mbs.mkdir(path, pathInfo.Mode())
+					}
+
+					for _, dirItem := range dirContent { // Percorre os conteúdos do diretório
+						mbs.sync(path + string(os.PathSeparator) + dirItem.Name()) // Sincroniza esses conteúdos
+					}
+				}
+			} else {
+				return dirErr // Retorna o erro
+			}
+		} else { // Se não é um diretório, então, é um arquivo
+
+			if existsPath {
+				// Verifica se o arquivo não foi modificado
+				if pathInfo.ModTime().Equal(value.ModifiedAt) {
+					return nil // Não faz nada
+				}
+			}
+
+			copyErr := mbs.copy(path, pathInfo.Mode())
+
+			if copyErr != nil {
+				return copyErr
+			}
+
+			metadata := FileMetadata{
+				Path:       path,
+				Filename:   pathInfo.Name(),
+				IsDir:      pathInfo.IsDir(),
+				ModifiedAt: time.Now(),
+			}
+
+			mbs.backupHistory.Insert(path, metadata) // Atualiza o histórico de Backup
+		}
+	}
+	return nil
+}
+
+func (mbs *MyBackupSystem) mkdir(path string, fileMode os.FileMode) error {
+	after, _ := strings.CutPrefix(path, mbs.backupSrc) // Remove todo caminho até o diretório de backup
+	dirPath := mbs.backupDst + mbs.srcDir + after      // Monta o caminho correto para a cópia
+	err := os.Mkdir(dirPath, fileMode)
+
+	return err
+}
+
+func (mbs *MyBackupSystem) copy(path string, fileMode os.FileMode) error {
+	fileData, err := os.ReadFile(path)
+	if err != nil {
+		return errors.New("Falha ao ler o conteúdo de " + path)
+	}
+	after, _ := strings.CutPrefix(path, mbs.backupSrc) // Remove todo caminho até o diretório de backup
+	bPath := mbs.backupDst + mbs.srcDir + after  // Monta o caminho correto para a cópia
+
+	err = os.WriteFile(bPath, fileData, fileMode)
+	return err
+}
+
+func (mbs* MyBackupSystem) setSyncedAt() {
+	mbs.syncedAt = time.Now()
 }
