45fc7ea749a141c29c80e8061ae68460d407a5aa
afmireski <afmireski@outlook.com>
2023-09-18 16:08:27 -0300

    Remove code from main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,21 +8,5 @@ import (
 )
 
 func main() {
-	src := "/home/afmireski/Área de trabalho/tmp"
-	dst := "/home/afmireski/Documentos/BCC/p4/aspectos-linguagens-programacao/trabalhos/trabalho01/alp-backup-system/backup/"
-
-	bs := backup_system.InitBackupSystem(dst)
-	bs.SetBackupSrc(src)
-	bs.Print()
-	err := bs.Sync()
-	if err != nil {
-		log.Fatal(err)
-	}
-	bs.Print()
-	time.Sleep(1 * time.Minute)
-	err = bs.Sync()
-	if err != nil {
-		log.Fatal(err)
-	}
-	bs.Print()
+	
 }
